1d5740277228219e7a29b445d536906d6178e3e3
Younghan Yonn <dudgks1172@naver.com>
2024-11-25 13:00:25 +0900

    [#4] feat(OrderRepository) : 주문 현황 리스트 기능 구현

--- src/main/java/org/example/springplusteam/domain/order/OrderRepository.java
+++ src/main/java/org/example/springplusteam/domain/order/OrderRepository.java
@@ -1,4 +1,12 @@
 package org.example.springplusteam.domain.order;
 
-public interface OrderRepository {
+import org.example.springplusteam.common.security.AuthUser;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Page<Order> findAllByUser(Long userId, Pageable pageable);
 }
