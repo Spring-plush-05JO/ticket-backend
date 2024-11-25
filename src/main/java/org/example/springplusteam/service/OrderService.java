d7b89c523f12a0f920923bb7219fbad17d1b9b75
Younghan Yonn <dudgks1172@naver.com>
2024-11-25 13:00:31 +0900

    [#4] feat(OrderService) : 주문 현황 리스트 기능 구현

--- src/main/java/org/example/springplusteam/service/OrderService.java
+++ src/main/java/org/example/springplusteam/service/OrderService.java
@@ -1,4 +1,44 @@
 package org.example.springplusteam.service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.springplusteam.domain.order.Order;
+import org.example.springplusteam.domain.order.OrderRepository;
+import org.example.springplusteam.dto.order.resp.OrderGetRespDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class OrderService {
+    private final OrderRepository orderRepository;
+
+    @Transactional
+    public Page<OrderGetRespDto> getOrders(
+            Long userId,
+            int page,
+            int limit,
+            String criteria
+    ) {
+        Pageable pageable = PageRequest.of(page, limit, Sort.by(Sort.Direction.DESC, criteria));
+        Page<Order> orders = orderRepository.findAllByUser(userId, pageable);
+
+        Page<OrderGetRespDto> response = orders.map(order ->
+                new OrderGetRespDto(
+                        order.getId(),
+                        order.getProduct().getName(),
+                        order.getProduct().getPrice(),
+                        order.getDeliveryStatus(),
+                        order.getCreatedAt(),
+                        order.getModifiedAt()
+                )
+        );
+
+        return response;
+    }
+
 }
+
