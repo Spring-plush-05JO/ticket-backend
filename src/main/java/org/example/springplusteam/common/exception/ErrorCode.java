b41716c5a6ada765e4cb251833bcce14eb047ce5
Younghan Yonn <dudgks1172@naver.com>
2024-11-25 16:12:01 +0900

    [#12] refactor(ErrorCode)PRODUCT_NOT_FOUND

--- src/main/java/org/example/springplusteam/common/exception/ErrorCode.java
+++ src/main/java/org/example/springplusteam/common/exception/ErrorCode.java
@@ -10,7 +10,7 @@ public enum ErrorCode {
     TOKEN_NOT_FOUND(401, "토큰 정보를 찾을 수 없습니다."),
     INVALID_TOKEN_ERROR(403, "토큰 정보가 잘못되었습니다."),
     EXISTS_EMAIL(400, "존재하는 이메일 입니다."),
-    PRODUCT_NOT_FOUND(504, "상품을 찾을 수 없습니다.");
+    PRODUCT_NOT_FOUND(404, "상품을 찾을 수 없습니다.");
 
     private final int status;
     private final String message;
