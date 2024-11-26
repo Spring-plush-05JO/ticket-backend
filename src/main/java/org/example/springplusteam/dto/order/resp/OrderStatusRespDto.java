9375a4eb531e5c16ee5a5b8a85993857dc6979b3
Younghan Yonn <dudgks1172@naver.com>
2024-11-27 00:23:28 +0900

    [#5] feat(OrderStatusRespDto) @Builder 추가

--- src/main/java/org/example/springplusteam/dto/order/resp/OrderStatusRespDto.java
+++ src/main/java/org/example/springplusteam/dto/order/resp/OrderStatusRespDto.java
@@ -1,11 +1,12 @@
 package org.example.springplusteam.dto.order.resp;
 
+import lombok.Builder;
 import lombok.Getter;
 import org.example.springplusteam.domain.order.DeliveryStatus;
 
 import java.time.LocalDateTime;
 
-
+@Builder
 @Getter
 public class OrderStatusRespDto {
     private Long orderId;
