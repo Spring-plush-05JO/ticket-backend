c8b92ffb9e285ab180ab803d39aa081bc68e32c4
Younghan Yonn <dudgks1172@naver.com>
2024-11-27 00:55:04 +0900

    [#5] refactor(OrderStatusRespDto) 빌더 리팩토링

--- src/main/java/org/example/springplusteam/dto/order/resp/OrderStatusRespDto.java
+++ src/main/java/org/example/springplusteam/dto/order/resp/OrderStatusRespDto.java
@@ -1,18 +1,17 @@
-package org.example.springplusteam.dto.order.resp;
+    package org.example.springplusteam.dto.order.resp;
 
-import lombok.Builder;
-import lombok.Getter;
-import org.example.springplusteam.domain.order.DeliveryStatus;
+    import lombok.Builder;
+    import lombok.Getter;
+    import org.example.springplusteam.domain.order.DeliveryStatus;
 
-import java.time.LocalDateTime;
-
-@Builder
-@Getter
-public class OrderStatusRespDto {
-    private Long orderId;
-    private String name;
-    private int price;
-    private DeliveryStatus status;
-    private LocalDateTime createdAt;
-    private LocalDateTime modifiedAt;
-}
+    import java.time.LocalDateTime;
+    @Builder
+    @Getter
+    public class OrderStatusRespDto {
+        private Long orderId;
+        private String name;
+        private int price;
+        private DeliveryStatus status;
+        private LocalDateTime createdAt;
+        private LocalDateTime modifiedAt;
+    }
