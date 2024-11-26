eb26e1364b2560d56313fd2cd94a5aea90192a1a
Younghan Yonn <dudgks1172@naver.com>
2024-11-26 20:36:55 +0900

    [#5] refactor(Order) 띄어쓰기 정리

--- src/main/java/org/example/springplusteam/domain/order/Order.java
+++ src/main/java/org/example/springplusteam/domain/order/Order.java
@@ -8,23 +8,23 @@ import org.example.springplusteam.common.BaseEntity;
 import org.example.springplusteam.domain.product.Product;
 import org.example.springplusteam.domain.user.User;
 
-    @Entity
-    @Getter
-    @Table(name = "orders")
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    public class Order extends BaseEntity {
+@Entity
+@Getter
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseEntity {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "user_id")
-        private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
-        @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "product_id")
-        private Product product;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
 
     @Enumerated(EnumType.STRING)
     private DeliveryStatus deliveryStatus;
