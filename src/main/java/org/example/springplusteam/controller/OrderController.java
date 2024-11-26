7730220fce740444469e59e062d92f94f336aea8
Younghan Yonn <dudgks1172@naver.com>
2024-11-26 17:29:06 +0900

    [#4] feat(OrderController) Get메서드 생성

--- src/main/java/org/example/springplusteam/controller/OrderController.java
+++ src/main/java/org/example/springplusteam/controller/OrderController.java
@@ -2,18 +2,17 @@ package org.example.springplusteam.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.springplusteam.common.security.AuthUser;
-import org.example.springplusteam.dto.order.resp.OrderGetRespDto;
 import org.example.springplusteam.dto.order.OrderCreateRespDto;
+import org.example.springplusteam.dto.order.resp.OrderSearchRespDto;
 import org.example.springplusteam.service.OrderService;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,15 +26,21 @@ public class OrderController {
         Long userId = user.getId();
         OrderCreateRespDto orderCreateRespDto = orderService.createOrder(userId, productId);
         return ResponseEntity.status(HttpStatus.CREATED).body(orderCreateRespDto);
+    }
+
     @GetMapping("/api/v1/orders")
-    public ResponseEntity<Page<OrderGetRespDto>> getOrders(
-            @AuthenticationPrincipal AuthUser authUser,
-            @RequestParam(defaultValue = "1", value = "page") int page,
-            @RequestParam(defaultValue = "10", value = "limit") int limit,
-            @RequestParam(defaultValue = "modifiedAt", value = "criteria") String criteria
+    public ResponseEntity<Page<OrderSearchRespDto>> getOrders(
+            @AuthenticationPrincipal AuthUser user,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int limit,
+            @RequestParam(defaultValue = "modifiedAt") String sortBy,
+            @RequestParam(defaultValue = "desc") String direction
 
     ){
-        Page<OrderGetRespDto> respDtos = orderService.getOrders(authUser.getId(), page, limit, criteria);
+        Long userId = user.getId();
+        Sort sort = Sort.by(Sort.Direction.fromString(direction), sortBy);
+        Pageable pageable = PageRequest.of(page, limit, sort);
+        Page<OrderSearchRespDto> respDtos = orderService.getOrders(userId, pageable);
         return ResponseEntity.status(HttpStatus.OK).body(respDtos);
     }
 }
