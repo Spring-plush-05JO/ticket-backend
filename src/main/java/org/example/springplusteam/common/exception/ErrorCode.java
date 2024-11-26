da4e64ab924671db651353b4d26e9f4ced9f0c56
Younghan Yonn <dudgks1172@naver.com>
2024-11-27 00:21:34 +0900

    [#5] refactor(ErrorCode) 소문자 정정 및 ORDER_BAD_REQUEST 추가

--- src/main/java/org/example/springplusteam/common/exception/ErrorCode.java
+++ src/main/java/org/example/springplusteam/common/exception/ErrorCode.java
@@ -10,7 +10,9 @@ public enum ErrorCode {
     TOKEN_NOT_FOUND(401, "토큰 정보를 찾을 수 없습니다."),
     INVALID_TOKEN_ERROR(403, "토큰 정보가 잘못되었습니다."),
     EXISTS_EMAIL(400, "존재하는 이메일 입니다."),
-    PRODUCT_NOT_FOUND(404, "상품을 찾을 수 없습니다.");
+    PRODUCT_NOT_FOUND(404, "상품을 찾을 수 없습니다."),
+    ORDER_NOT_FOUND(404, "해당 주문을 찾을 수 없습니다."),
+    ORDER_BAD_REQUEST(400, "잘못된 주문 상태 변경 요청입니다.");
 
     private final int status;
     private final String message;
