151aa4af8a2fc64814f1606a537f6ba2acaf5868
Younghan Yonn <dudgks1172@naver.com>
2024-11-26 17:29:41 +0900

    [#4] feat(OrderService) Get메서드 생성

--- src/main/java/org/example/springplusteam/service/OrderService.java
+++ src/main/java/org/example/springplusteam/service/OrderService.java
@@ -5,16 +5,15 @@ import org.example.springplusteam.common.exception.CustomApiException;
 import org.example.springplusteam.common.exception.ErrorCode;
 import org.example.springplusteam.domain.order.Order;
 import org.example.springplusteam.domain.order.OrderRepository;
+import org.example.springplusteam.domain.order.OrderRepositoryCustomImpl;
 import org.example.springplusteam.domain.product.Product;
 import org.example.springplusteam.domain.product.ProductRepository;
 import org.example.springplusteam.domain.user.User;
 import org.example.springplusteam.domain.user.UserRepository;
 import org.example.springplusteam.dto.order.OrderCreateRespDto;
-import org.example.springplusteam.dto.order.resp.OrderGetRespDto;
+import org.example.springplusteam.dto.order.resp.OrderSearchRespDto;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +25,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final ProductRepository productRepository;
     private final UserRepository userRepository;
+    private final OrderRepositoryCustomImpl orderRepositoryCustomImpl;
 
     @Transactional
     public OrderCreateRespDto createOrder(Long userId, Long productId) {
@@ -34,14 +34,6 @@ public class OrderService {
 
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new CustomApiException(ErrorCode.PRODUCT_NOT_FOUND));
-    public Page<OrderGetRespDto> getOrders(
-            Long userId,
-            int page,
-            int limit,
-            String criteria
-    ) {
-        Pageable pageable = PageRequest.of(page, limit, Sort.by(Sort.Direction.DESC, criteria));
-        Page<Order> orders = orderRepository.findAllByUserId(userId, pageable);
 
         Order order = new Order(user, product);
 
@@ -56,7 +48,14 @@ public class OrderService {
         );
 
         return respDto;
-        return response;
+    }
+
+    @Transactional
+    public Page<OrderSearchRespDto> getOrders(
+            Long userId,
+            Pageable pageable
+    ) {
+        return orderRepositoryCustomImpl.searchOrdersByUser(userId, pageable);
     }
 }
 
