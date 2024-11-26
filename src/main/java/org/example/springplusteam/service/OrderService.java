e988f6a60089d81fa8bbc2fcf86d367e2bc22c31
Younghan Yonn <dudgks1172@naver.com>
2024-11-27 00:23:54 +0900

    [#5] feat(OrderService) changeOrder메서드 추가

--- src/main/java/org/example/springplusteam/service/OrderService.java
+++ src/main/java/org/example/springplusteam/service/OrderService.java
@@ -3,6 +3,7 @@ package org.example.springplusteam.service;
 import lombok.RequiredArgsConstructor;
 import org.example.springplusteam.common.exception.CustomApiException;
 import org.example.springplusteam.common.exception.ErrorCode;
+import org.example.springplusteam.domain.order.DeliveryStatus;
 import org.example.springplusteam.domain.order.Order;
 import org.example.springplusteam.domain.order.OrderRepository;
 import org.example.springplusteam.domain.order.OrderRepositoryCustomImpl;
@@ -10,14 +11,16 @@ import org.example.springplusteam.domain.product.Product;
 import org.example.springplusteam.domain.product.ProductRepository;
 import org.example.springplusteam.domain.user.User;
 import org.example.springplusteam.domain.user.UserRepository;
-import org.example.springplusteam.dto.order.OrderCreateRespDto;
+import org.example.springplusteam.dto.order.req.OrderStatusReqDto;
+import org.example.springplusteam.dto.order.resp.OrderCreateRespDto;
 import org.example.springplusteam.dto.order.resp.OrderSearchRespDto;
+import org.example.springplusteam.dto.order.resp.OrderStatusRespDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.example.springplusteam.common.exception.ErrorCode.USER_NOT_FOUND;
+import static org.example.springplusteam.common.exception.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -56,6 +59,30 @@ public class OrderService {
             Pageable pageable
     ) {
         return orderRepositoryCustomImpl.searchOrdersByUser(userId, pageable);
+    }
+
+    @Transactional
+    public OrderStatusRespDto changeOrder(Long userId, Long orderId, OrderStatusReqDto reqDto) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new CustomApiException(ORDER_NOT_FOUND));
+
+        if (order.getDeliveryStatus().equals(DeliveryStatus.COMPLETED)) {
+            throw new CustomApiException(ORDER_BAD_REQUEST);
+        }
+
+        order.update(reqDto.getStatus());
+
+        orderRepository.saveAndFlush(order);
+        OrderStatusRespDto respDto = OrderStatusRespDto.builder()
+                .orderId(orderId)
+                .name(order.getProduct().getName())
+                .price(order.getProduct().getPrice())
+                .status(order.getDeliveryStatus())
+                .createdAt(order.getCreatedAt())
+                .modifiedAt(order.getModifiedAt())
+                .build();
+
+        return respDto;
     }
 }
 
