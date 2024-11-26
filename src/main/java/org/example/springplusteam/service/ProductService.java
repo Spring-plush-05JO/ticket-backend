9cada390962f21d6f7d71d9b9ef5aef3b0d2e8b4
정현우 <jhw8449@gmail.com>
2024-11-26 19:32:06 +0900

    [FEAT] 상품 리스트 조회 기능 구현 (#25)
    * [#16] refactor(ProductController, ProductService, QuerydslConfig): 상품 리스트 조회 기능 구현
    
    * [#16] refactor(ProductController, ProductService, QuerydslConfig): 상품 리스트 조회 수정

--- src/main/java/org/example/springplusteam/service/ProductService.java
+++ src/main/java/org/example/springplusteam/service/ProductService.java
@@ -1,15 +1,21 @@
 package org.example.springplusteam.service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.example.springplusteam.domain.product.Product;
 import org.example.springplusteam.domain.product.ProductRepository;
 import org.example.springplusteam.domain.product.QProduct;
 import org.example.springplusteam.dto.product.req.ProductCreateReqDto;
 import org.example.springplusteam.dto.product.resp.ProductCreateRespDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class ProductService {
 
 	private final ProductRepository productRepository;
+	private final JPAQueryFactory jpaQueryFactory;
 
 	public ProductCreateRespDto createProduct(ProductCreateReqDto reqDto) {
 		Product product = Product.builder()
@@ -44,5 +51,16 @@ public class ProductService {
 			product.getPerformance_period(),
 			product.getViewCount()
 		);
+	}
+
+	public Page<ProductCreateRespDto> listProducts(Pageable pageable) {
+		List<Product> products = productRepository.findAllWithPagination(pageable);
+		long total = productRepository.countProducts();
+
+		List<ProductCreateRespDto> content = products.stream()
+			.map(ProductCreateRespDto::new)
+			.collect(Collectors.toList());
+
+		return new PageImpl<>(content, pageable, total);
 	}
 }
