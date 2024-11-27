6dc7417e87780774997fde8da2d9a0389a62ba11
Geonu Yu <dbrjdn2051@naver.com[C>
2024-11-27 11:23:54 +0900

    :sparkles: [#20] feat(TicketService) : ticket 예매 서비스 로직 구현

--- src/main/java/org/example/springplusteam/service/TicketService.java
+++ src/main/java/org/example/springplusteam/service/TicketService.java
@@ -1,15 +1,20 @@
 package org.example.springplusteam.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.val;
 import org.example.springplusteam.common.exception.CustomApiException;
 import org.example.springplusteam.common.exception.ErrorCode;
+import org.example.springplusteam.domain.myTicket.MyTicket;
+import org.example.springplusteam.domain.myTicket.MyTicketRepository;
 import org.example.springplusteam.domain.ticket.Ticket;
 import org.example.springplusteam.domain.ticket.TicketRepository;
+import org.example.springplusteam.domain.user.User;
+import org.example.springplusteam.domain.user.UserRepository;
 import org.example.springplusteam.dto.ticket.req.TicketCreateReqDto;
 import org.example.springplusteam.dto.ticket.resp.TicketCreateRespDto;
+import org.example.springplusteam.dto.ticket.resp.TicketReserveRespDto;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +22,8 @@ public class TicketService {
 
     private final TicketRepository ticketRepository;
     private final RedisTemplate<String, String> redisTemplate;
+    private final UserRepository userRepository;
+    private final MyTicketRepository myTicketRepository;
 
     public TicketCreateRespDto addTicket(TicketCreateReqDto dto) {
         Ticket savedTicket = ticketRepository.save(TicketCreateReqDto.from(dto));
@@ -32,5 +39,34 @@ public class TicketService {
 
         redisTemplate.delete(key);
         redisTemplate.opsForValue().set(key + ":seatCount", String.valueOf(ticket.getQuantity()));
+    }
+
+    @Transactional
+    public TicketReserveRespDto reserveTicket(Long ticketId, Long userId, String seatNumber) {
+        String key = "ticket:seat:" + ticketId;
+
+        Ticket ticket = ticketRepository.findTicketWithLock(ticketId)
+                .orElseThrow(() -> new CustomApiException(ErrorCode.TICKET_NOT_FOUND));
+
+        if (ticket.getQuantity() <= 0) {
+            throw new CustomApiException(ErrorCode.NO_SEAT_AVAILABLE);
+        }
+
+        Long reserveSeat = redisTemplate.opsForSet().add(key, seatNumber);
+        if (reserveSeat != 1L) {
+            throw new CustomApiException(ErrorCode.ALREADY_RESERVE_SEAT);
+        }
+
+        Long decrement = redisTemplate.opsForValue().decrement(key + ":seatCount");
+        if (decrement < 0) {
+            throw new CustomApiException(ErrorCode.NO_SEAT_AVAILABLE);
+        }
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomApiException(ErrorCode.USER_NOT_FOUND));
+        ticket.updateQuantities();
+        myTicketRepository.save(new MyTicket(user, ticket));
+        ticketRepository.save(ticket);
+        return new TicketReserveRespDto(user.getId(), seatNumber);
     }
 }
