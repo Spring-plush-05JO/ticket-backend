d450f74d2f4f674dff3975b190c2ebe017bb8d7a
Geonu Yu <dbrjdn2051@naver.com[C>
2024-11-27 11:23:06 +0900

    :sparkles: [#20] feat(TicketRepository) : ticketRepository findById Rock 을 이용하여 구현

--- src/main/java/org/example/springplusteam/domain/ticket/TicketRepository.java
+++ src/main/java/org/example/springplusteam/domain/ticket/TicketRepository.java
@@ -1,6 +1,16 @@
 package org.example.springplusteam.domain.ticket;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select t from Ticket t where t.Id = :id")
+    Optional<Ticket> findTicketWithLock(@Param("id") Long id);
 }
