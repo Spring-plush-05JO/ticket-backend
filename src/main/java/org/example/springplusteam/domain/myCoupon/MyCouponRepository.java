1c560cf56b0a08e9de5f4ed0b05eb3f8815a58bb
Younghan Yonn <dudgks1172@naver.com>
2024-11-28 00:10:10 +0900

    [#38] feat(MyCouponRepository) 쿠폰 발급 기능 구현

---
+++ src/main/java/org/example/springplusteam/domain/myCoupon/MyCouponRepository.java
@@ -0,0 +1,6 @@
+package org.example.springplusteam.domain.myCoupon;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MyCouponRepository extends JpaRepository<MyCoupon, Long> {
+}
