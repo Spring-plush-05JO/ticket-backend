d57b4c7745df7e7d2ebbf3745825e424d6a2bd26
11ioo <11ioo@napalt.co.kr>
2024-11-28 09:34:38 +0900

    [#6] feat: 공연 조회서비스 구현

--- src/main/java/org/example/springplusteam/domain/performances/Performances.java
+++ src/main/java/org/example/springplusteam/domain/performances/Performances.java
@@ -1,11 +1,12 @@
 package org.example.springplusteam.domain.performances;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.io.Serializable;
-import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,20 +14,28 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
+@Table(name="performances")
 public class Performances implements Serializable {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
+  private Integer id;
+  @Column(name = "name")
   private String name;
+  @Column(name="genre")
   private String genre;
+  @Column(name="place")
   private String place ;
+  @Column(name="main_address")
   private String mainAddress;
+  @Column(name="sub_address")
   private String subAddress;
-  private LocalDate startData;
-  private LocalDate endData;
+  @Column(name = "start_data")
+  private String startData;
+  @Column(name = "end_data")
+  private String endData;
 
-  public Performances(String name, String genre, String place, String mainAddress, String subAddress, LocalDate startData, LocalDate endData) {
+  public Performances(String name, String genre, String place, String mainAddress, String subAddress, String startData, String endData) {
     this.name = name;
     this.genre = genre;
     this.place = place;
@@ -34,5 +43,15 @@ public class Performances implements Serializable {
     this.subAddress = subAddress;
     this.startData = startData;
     this.endData = endData;
+  }
+  public Performances(String name, String genre, String place, String mainAddress, String subAddress, String startData, String endData, Integer id) {
+    this.name = name;
+    this.genre = genre;
+    this.place = place;
+    this.mainAddress = mainAddress;
+    this.subAddress = subAddress;
+    this.startData = startData;
+    this.endData = endData;
+    this.id = id;
   }
 }
