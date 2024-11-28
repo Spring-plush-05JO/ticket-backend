b784360d88307c61320171abc29dc72e4db02d4e
Younghan Yonn <dudgks1172@naver.com>
2024-11-28 19:08:44 +0900

    [#38] feat(CouponRepository) 비관적 잠금 구현

--- src/main/java/org/example/springplusteam/domain/coupon/CouponRepository.java
+++ src/main/java/org/example/springplusteam/domain/coupon/CouponRepository.java
@@ -1,6 +1,15 @@
 package org.example.springplusteam.domain.coupon;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT c FROM Coupon c WHERE c.id =: id")
+    Optional<Coupon> findByCouponId(@Param("id") Long id);
 }
